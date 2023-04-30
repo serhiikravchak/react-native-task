db9cdb6d6a9d7feb2b7c605ce528eaa210fdb9b3
serhiikravchak <105275364+serhiikravchak@users.noreply.github.com>
2023-04-30 16:23:18 +0200

    dependencies installed

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+      <Text style={styles.text}>I`m first task in React Native course in Go IT Academy</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text:{
+    color: '#f0f'
+  },
+});
